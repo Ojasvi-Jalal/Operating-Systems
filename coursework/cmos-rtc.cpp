1785d96d53335b5212a8e3ee33f05762bc53d1a8
Ojasvi Jalal <s1612970@sms.ed.ac.uk>
2019-03-27 20:49:03 +0000

    hour-conversion

--- coursework/cmos-rtc.cpp
+++ coursework/cmos-rtc.cpp
@@ -75,6 +75,22 @@ public:
 	}
 
 	/*
+	//interrogating register B to check whether the value contained within the registers is in
+	//24-hour or 12-hour format
+	*/
+	bool is_twelve(){
+		__outb(cmos_write, 0x0B);
+		//check bit 2 of register B
+		uint8_t hour_format_24 = __inb(cmos_read)&(1<<1);
+
+		//if bit 1 of register B is set then the hour is in 24-hour format else in 12-hour format
+		if(hour_format_24)
+			return false;
+		else
+			return true;
+	}
+
+	/*
 	//reads the byte at a given offset (reads the values of a register)
 	*/
 	unsigned short read_register(int reg) {
@@ -91,6 +107,18 @@ public:
 		else
 			return RTC_register;
 	}
+
+	/*
+	//reads the byte at the given offset for the hour register
+	//(reads the values of the hour register)
+	*/
+	unsigned short read_hour() {
+		//activate offset "reg"
+		__outb(cmos_write,0x04);
+		//return the register value
+		return __inb(cmos_read);
+	}
+
 	/**
 	 * Interrogates the RTC to read the current date & time.
 	 * @param tp Populates the tp structure with the current data & time, as
@@ -110,18 +138,42 @@ public:
 		//read the real time clock registers
 		second	= read_register(0x00);
   	minute	= read_register(0x02);
-  	hour	= read_register(0x04);
-  	day 	= read_register(0x07);
+  	hour		= read_hour();
+  	day 		= read_register(0x07);
   	month 	= read_register(0x08);
-  	year 	= read_register(0x09);
+  	year 		= read_register(0x09);
+
+		//12 hour clock to 24 hour clock conversion if NECESSARY
+		if(is_twelve()){
+			//if the hour is pm, then the 0x80 bit is set on the hour byte
+			bool pm = hour>>7;
+
+			//mask the 0x80 bit off
+			hour = 0x7F&hour; // 0111 1111
+
+			//check if bcd and convert it to binary in this case
+			if(is_bcd())
+				hour = ((hour&0x0F)+(hour/16)*10);
+
+			if(pm)
+				hour += 12;
+			//when midnight
+			else if(hour == 12)
+				hour = 0;
+		}
+
+		else{
+			if(is_bcd())
+				hour = ((hour&0x0F)+(hour/16)*10);
+		}
 
 		//filling in the fields of RTCTimePoint, the structure provided
-		tp.seconds			=  second;
-		tp.minutes 			=  minute;
-		tp.hours			=  hour;
+		tp.seconds				=  second;
+		tp.minutes 				=  minute;
+		tp.hours					=  hour;
 		tp.day_of_month		=  day;
-		tp.month			=  month;
-		tp.year				=  year;
+		tp.month					=  month;
+		tp.year						=  year;
 	}
 };
 
